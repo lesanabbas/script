1dc7b5e8e86cc759b3a67b511e38fb448ddaccc2
Lesan Abbas <63160640+lesanabbas@users.noreply.github.com>
2024-10-07 21:51:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,8 @@ def check_website(url):
         print(f"{time.ctime()}: {url} is down! Exception: {e}")
 
 if __name__ == "__main__":
-    url_to_check = "https://social-media-9nzi.onrender.com/"  # Replace with the URL you want to check
-    while True:
-        check_website(url_to_check)
-        time.sleep(60)  # Wait for 60 seconds
+    # url_to_check = "https://social-media-9nzi.onrender.com/"  # Replace with the URL you want to check
+    # while True:
+    #     check_website(url_to_check)
+    #     time.sleep(60)  # Wait for 60 seconds
+    print("Hello, Lesan Abbas")
